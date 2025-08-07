566f10fe618c0cdb7b40d380bfc9767c8a4741b9
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-08-07 10:53:08 +0530

    feat : Neon database configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className='text-4xl front-bold text-green-500'>
-      Welcome to the MeetAI app!
-    </div>
+    <Button>
+      Click me
+    </Button>
   );
 }
