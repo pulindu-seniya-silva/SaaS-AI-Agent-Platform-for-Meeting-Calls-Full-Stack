7cfc19f6610f7098d57ae5a0cf3c0802499a331d
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-08-11 12:55:25 +0530

    feat : user submited successfully (form and configurations added)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import {authClient} from "@/lib/auth-client";
 
 export default function Home() {
 
@@ -11,12 +12,27 @@ export default function Home() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    }, {
+      onError: () => {
+        window.alert("Something went wrong")
+      },
+      onSuccess: () => {
+        window.alert("Success")
+      }
+    })
+  }
+
   return (
     <div className="p-4 flex flex-col gap-y-4">
       <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)}/>
       <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
       <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-      <Button>
+      <Button onClick={onSubmit}>
         Create User
       </Button>
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
+import * as schema from "@/db/schema"; // your schema
  
 export const auth = betterAuth({
     emailAndPassword: {
@@ -8,5 +9,8 @@ export const auth = betterAuth({
   },
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
+        schema: {
+            ...schema,
+        }
     }),
 });
