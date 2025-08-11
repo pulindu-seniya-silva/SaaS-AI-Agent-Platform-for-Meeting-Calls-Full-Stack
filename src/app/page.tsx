602bad1ff05c390948a5e5811e904cb7ad45fe06
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-08-11 13:05:52 +0530

    feat: manage existing user sessions and users that already exist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ import {authClient} from "@/lib/auth-client";
 
 export default function Home() {
 
+   const { 
+        data: session, 
+    } = authClient.useSession() 
+
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -25,6 +29,17 @@ export default function Home() {
         window.alert("Success")
       }
     })
+  }
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>
+          Sign Out
+        </Button>
+      </div>
+    )
   }
 
   return (
