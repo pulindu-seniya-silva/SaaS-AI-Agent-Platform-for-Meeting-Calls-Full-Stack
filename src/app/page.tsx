0da1633d5dfc2f8a2e056abbe20a4f6a4d8330bb
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-08-11 12:44:41 +0530

    feat : form for user sign up test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <Button>
-      Click me
-    </Button>
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)}/>
+      <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+      <Button>
+        Create User
+      </Button>
+
+    </div>
   );
 }
